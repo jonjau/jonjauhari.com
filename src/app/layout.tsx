2c2573c9bd367d1d76884793f165e1be596f9ca9
jonjau <jonjau3q@gmail.com>
2023-10-08 00:32:49 +1100

    factor out mobile layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,125 +53,172 @@ export default function RootLayout({
       className={`${sourceSans.variable} ${sourceSerif.variable}`}
     >
       <body className="flex min-h-screen flex-col bg-stone-800 text-amber-100">
-        <header className="fixed left-0 right-0 z-10 sm:absolute">
-          <nav className=" overflow-x-auto bg-stone-900 p-4 sm:bg-transparent sm:px-4 sm:py-12">
-            <div className="mx-auto flex items-center sm:max-w-6xl">
-              <Link href="/" className={clsx("flex-0", ...uiLinkClasses)}>
-                <div className="mb-1 flex justify-center">
-                  <span className="flex items-center justify-center">
-                    <FaSquareFull className="rounded-sm text-2xl text-[#197060]" />
-                  </span>
-                  <span className="mx-3 hidden text-2xl font-bold sm:inline ">
-                    Jonathan Jauhari
-                  </span>
-                </div>
-              </Link>
-              <div className="flex flex-1 items-center justify-end">
-                {[
-                  { linkText: "Blog", href: "/blog" },
-                  { linkText: "Projects", href: "/projects" },
-                  { linkText: "About", href: "/about" },
-                ].map(({ linkText, href }) => (
-                  <Link
-                    key={href}
-                    href={href}
-                    className={clsx(
-                      "hidden",
-                      "mx-2",
-                      "sm:block",
-                      "px-4",
-                      "my-1",
-                      "text-2xl",
-                      "font-bold",
-                      ...uiLinkClasses,
-                    )}
-                  >
-                    {linkText}
-                  </Link>
-                ))}
-                <div className="z-20 flex sm:hidden">
-                  <input
-                    type="checkbox"
-                    className="peer hidden"
-                    id="checkbox-toggle"
-                  ></input>
-                  <label
-                    htmlFor="checkbox-toggle"
-                    className="z-30 block select-none text-3xl text-amber-200 peer-checked:hidden"
-                  >
-                    <FaBars />
-                  </label>
-                  <label
-                    htmlFor="checkbox-toggle"
-                    className="z-30 hidden select-none text-3xl text-amber-200 peer-checked:block"
-                  >
-                    <FaTimes />
-                  </label>
-                  <div className="absolute -right-1/3 top-0 flex h-screen w-1/3 translate-x-0 flex-col justify-center bg-stone-950 text-center  transition-transform peer-checked:-translate-x-full">
-                    {[
-                      { linkText: "Blog", href: "/blog" },
-                      { linkText: "Projects", href: "/projects" },
-                      { linkText: "About", href: "/about" },
-                    ].map(({ linkText, href }) => (
-                      <Link
-                        key={href}
-                        href={href}
-                        className={clsx(
-                          "mx-2",
-                          "sm:block",
-                          "my-4",
-                          "px-1",
-                          "text-2xl",
-                          "font-bold",
-                          ...uiLinkClasses,
-                        )}
-                      >
-                        {linkText}
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </header>
+        <Header />
         <main className="sm:flex sm:flex-auto sm:flex-col">
           <div className="mx-auto px-2 pt-20 sm:max-w-3xl sm:px-6 sm:pt-40">
             {children}
           </div>
         </main>
-        <footer className="mt-10">
-          <div className="mx-auto flex flex-col items-center pb-8 sm:max-w-3xl sm:flex-row">
-            <span className="inline-flex items-center">
-              © {new Date().getFullYear()} Jonathan Jauhari
-            </span>
-            <div className="flex items-center justify-center sm:ml-auto">
-              {[
-                { linkIcon: <FaGithub />, href: "https://github.com/jonjau" },
-                {
-                  linkIcon: <FaLinkedin />,
-                  href: "https://www.linkedin.com/in/jonathanjauhari/",
-                },
-                {
-                  linkIcon: <FaEnvelope />,
-                  href:
-                    "\u006d\u0061\u0069\u006c\u0074\u006f\u003a" +
-                    "%6A%6F%6E%63%6A%61%75%68%61%72%69%40%67%6D%61" +
-                    "%69%6C%2E%63%6F%6D",
-                },
-              ].map(({ linkIcon, href }) => (
-                <Link
-                  key={href}
-                  href={href}
-                  className={clsx("p-2", "text-2xl", ...uiLinkClasses)}
-                >
-                  {linkIcon}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
 }
+
+const Header = () => (
+  <header className="fixed left-0 right-0 z-10 sm:absolute">
+    <nav
+      className={clsx([
+        "bg-stone-900",
+        "p-4",
+        "sm:bg-transparent",
+        "sm:px-4",
+        "sm:py-12",
+      ])}
+    >
+      <div className="mx-auto flex items-center sm:max-w-6xl">
+        <Link href="/" className={clsx("flex-0", ...uiLinkClasses)}>
+          <div className="mb-1 flex justify-center">
+            <span className="flex items-center justify-center">
+              <FaSquareFull className="rounded-sm text-2xl text-[#197060]" />
+            </span>
+            <span className="mx-3 hidden text-2xl font-bold sm:inline ">
+              Jonathan Jauhari
+            </span>
+          </div>
+        </Link>
+        <div className="flex flex-1 items-center justify-end">
+          <NavLinks
+            className={clsx([
+              "hidden",
+              "sm:block",
+              "mx-2",
+              "px-4",
+              "my-1",
+              "text-2xl",
+              "font-bold",
+              ...uiLinkClasses,
+            ])}
+          />
+          <MobileNav />
+        </div>
+      </div>
+    </nav>
+  </header>
+);
+
+const NavLinks = ({ className }: { className: string }) =>
+  [
+    { linkText: "Blog", href: "/blog" },
+    { linkText: "Projects", href: "/projects" },
+    { linkText: "About", href: "/about" },
+  ].map(({ linkText, href }) => (
+    <Link key={href} href={href} className={className}>
+      {linkText}
+    </Link>
+  ));
+
+const MobileNav = () => (
+  <div className="z-20 flex sm:hidden">
+    <input type="checkbox" className="peer hidden" id="checkbox-toggle" />
+    <label
+      htmlFor="checkbox-toggle"
+      className={clsx([
+        "z-30",
+        "block",
+        "select-none",
+        "text-3xl",
+        "text-amber-200",
+        "peer-checked:hidden",
+      ])}
+    >
+      <FaBars />
+    </label>
+    <label
+      htmlFor="checkbox-toggle"
+      className={clsx([
+        "z-30",
+        "hidden",
+        "select-none",
+        "text-3xl",
+        "text-amber-200",
+        "peer-checked:block",
+      ])}
+    >
+      <FaTimes />
+    </label>
+    <div
+      className={clsx([
+        "absolute",
+        "top-0",
+        "flex",
+        "flex-col",
+        "justify-center",
+        "bg-stone-950",
+        "text-center",
+        "-right-1/3",
+        "w-1/3",
+        "h-screen",
+        "translate-x-0",
+        "transition-transform",
+        "peer-checked:-translate-x-full",
+      ])}
+    >
+      <NavLinks
+        className={clsx([
+          "mx-2",
+          "sm:block",
+          "my-4",
+          "px-1",
+          "text-2xl",
+          "font-bold",
+          ...uiLinkClasses,
+        ])}
+      />
+    </div>
+  </div>
+);
+
+const Footer = () => (
+  <footer className="mt-10">
+    <div
+      className={clsx([
+        "mx-auto",
+        "flex",
+        "flex-col",
+        "items-center",
+        "pb-8",
+        "sm:max-w-3xl",
+        "sm:flex-row",
+      ])}
+    >
+      <span className="inline-flex items-center">
+        © {new Date().getFullYear()} Jonathan Jauhari
+      </span>
+      <div className="flex items-center justify-center sm:ml-auto">
+        {[
+          { linkIcon: <FaGithub />, href: "https://github.com/jonjau" },
+          {
+            linkIcon: <FaLinkedin />,
+            href: "https://www.linkedin.com/in/jonathanjauhari/",
+          },
+          {
+            linkIcon: <FaEnvelope />,
+            href:
+              "\u006d\u0061\u0069\u006c\u0074\u006f\u003a" +
+              "%6A%6F%6E%63%6A%61%75%68%61%72%69%40%67%6D%61" +
+              "%69%6C%2E%63%6F%6D",
+          },
+        ].map(({ linkIcon, href }) => (
+          <Link
+            key={href}
+            href={href}
+            className={clsx("p-2", "text-2xl", ...uiLinkClasses)}
+          >
+            {linkIcon}
+          </Link>
+        ))}
+      </div>
+    </div>
+  </footer>
+);
